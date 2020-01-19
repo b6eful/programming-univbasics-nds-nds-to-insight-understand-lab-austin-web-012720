253288144af06632c8df23d9520e25cfaef23cca
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 04:42:04 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,6 +8,6 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  directors_database[0][:movies].each {
+  directors_database[0][:movies].each { |movie|
   counter =+ 1
 end
